1a6a52c92a1244b28d68ad7617a35716f2196022
Laviniiabenvindo <laviniiabenvindo@gmail.com>
2023-10-07 15:31:29 -0300

    modificação na view-denuncia

--- routes/router.js
+++ routes/router.js
@@ -5,7 +5,7 @@ router.get("/", (request, response) => {
   return response.render("templates/pagina-inicial");
 });
 router.get("/denuncias/visualizar", (request, response) => {
-  return response.render("templates/home");
+  return response.render("templates/view-denuncia");
 });
 router.get("/user/adddenuncia/addinfo", (request, response) => {
   return response.render("templates/add_denuncia");
