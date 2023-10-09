7a621f36adbd3d9cfb70656de918e5c9d53e61dc
Lavínia Benvindo <laviniia.benvindo@gmail.com>
2023-10-09 17:37:56 -0300

    Conexção com o banco e criação da tabela Users

--- db/conn.js
+++ db/conn.js
@@ -1,15 +1,15 @@
-// const { Sequelize } = require("sequelize");
+const { Sequelize } = require("sequelize");
 
-// const sequelize = new Sequelize("", "", "", {
-//   host: "",
-//   port: "",
-//   dialect: "",
-// });
+const sequelize = new Sequelize("msp", "aluno_medio", "@lunoSenai23.", {
+  host: "127.0.0.1",
+  port: "3306",
+  dialect: "mysql",
+});
 
-// try {
-//   sequelize.authenticate();
-//   console.log("Conectado ao MYSQL!!!!");
-// } catch (error) {
-//   console.log(error);
-// }
-// module.exports = sequelize;
+try {
+  sequelize.authenticate();
+  console.log("Conectado ao MYSQL!!!!");
+} catch (error) {
+  console.log(error);
+}
+module.exports = sequelize;
